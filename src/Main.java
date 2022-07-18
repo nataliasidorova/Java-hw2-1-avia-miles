521a6be9d554c2c64469a419245bc9b422786cbb
nataliasidorova <nataliya.sidorova@ya.ru>
2022-07-18 18:52:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 100_000;
+        int bonus = 20;
+
+        int miles = ticket / bonus;
+
+        System.out.println(" Начисленные мили за покупку билета:" + miles);
+
+    }
+}
